b54dbf885967d22c62bafd0be49d7536e9ec0308
Makarevich Andrey <a2772.am@gmail.com>
2020-09-18 19:18:38 +0300

    auth start

---
+++ lib/app/services/firebase_db_srv.dart
@@ -0,0 +1,43 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:ayas_mobile/app/models/user.dart';
+
+class Database {
+  final Firestore _firestore = Firestore.instance;
+
+  Future<bool> createNewUser(UserModel user) async {
+    try {
+      await _firestore.collection("users").document(user.id).setData({
+        "name": user.name,
+        "email": user.email,
+      });
+      return true;
+    } catch (e) {
+      print(e);
+      return false;
+    }
+  }
+
+  Future<UserModel> getUser(String uid) async {
+    try {
+      DocumentSnapshot _doc = await _firestore.collection("users").document(uid).get();
+
+      return UserModel.fromDocumentSnapshot(documentSnapshot: _doc);
+    } catch (e) {
+      print(e);
+      rethrow;
+    }
+  }
+
+  Future<void> addTodo(String content, String uid) async {
+    try {
+      await _firestore.collection("users").document(uid).collection("todos").add({
+        'dateCreated': Timestamp.now(),
+        'content': content,
+        'done': false,
+      });
+    } catch (e) {
+      print(e);
+      rethrow;
+    }
+  }
+}
