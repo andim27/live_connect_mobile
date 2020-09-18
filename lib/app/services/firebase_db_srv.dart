41d62b3a3ed51653ea340f2bd5f475a314155538
Makarevich Andrey <a2772.am@gmail.com>
2020-09-18 19:23:04 +0300

    auth depricate fix

--- lib/app/services/firebase_db_srv.dart
+++ lib/app/services/firebase_db_srv.dart
@@ -2,11 +2,11 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:ayas_mobile/app/models/user.dart';
 
 class Database {
-  final Firestore _firestore = Firestore.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
   Future<bool> createNewUser(UserModel user) async {
     try {
-      await _firestore.collection("users").document(user.id).setData({
+      await _firestore.collection("users").doc(user.id).set({
         "name": user.name,
         "email": user.email,
       });
@@ -19,22 +19,9 @@ class Database {
 
   Future<UserModel> getUser(String uid) async {
     try {
-      DocumentSnapshot _doc = await _firestore.collection("users").document(uid).get();
+      DocumentSnapshot _doc = await _firestore.collection("users").doc(uid).get();
 
       return UserModel.fromDocumentSnapshot(documentSnapshot: _doc);
-    } catch (e) {
-      print(e);
-      rethrow;
-    }
-  }
-
-  Future<void> addTodo(String content, String uid) async {
-    try {
-      await _firestore.collection("users").document(uid).collection("todos").add({
-        'dateCreated': Timestamp.now(),
-        'content': content,
-        'done': false,
-      });
     } catch (e) {
       print(e);
       rethrow;
