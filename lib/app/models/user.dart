38b847b18b24da91d9992faa122f981c0a9ce46c
Makarevich Andrey <a2772.am@gmail.com>
2020-09-21 14:54:15 +0300

    api srv

--- lib/app/models/user.dart
+++ lib/app/models/user.dart
@@ -1,15 +1,34 @@
+import 'dart:math';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class UserModel {
   String id;
   String name;
   String email;
+  String avatarURL;
+  DateTime createdAt;
+  DateTime updatedAt;
 
-  UserModel({this.id, this.name, this.email});
+  UserModel({this.id, this.name, this.email, this.avatarURL, this.createdAt, this.updatedAt});
 
   UserModel.fromDocumentSnapshot({DocumentSnapshot documentSnapshot}) {
-    id = documentSnapshot.documentID;
-    name = documentSnapshot["name"];
-    email = documentSnapshot["email"];
+    id = documentSnapshot.id;
+    //name = documentSnapshot.["name"];
+    //email = documentSnapshot["email"];
+  }
+  String createRandomNum() {
+    int randomNum = Random().nextInt(999999);
+    return randomNum.toString();
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'email': email,
+      'name': name ?? email.substring(0, email.indexOf('@')) + createRandomNum(),
+      'avatarURL': avatarURL ?? 'https://via.placeholder.com/150',
+      'createdAt': createdAt ?? FieldValue.serverTimestamp(),
+      'updatedAt': updatedAt ?? FieldValue.serverTimestamp(),
+    };
   }
 }
