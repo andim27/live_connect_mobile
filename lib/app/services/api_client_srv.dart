6b858faa7beba1d98a32e66cfbebc08f52c6a95d
Makarevich Andrey <a2772.am@gmail.com>
2020-09-24 20:36:48 +0300

    api methods

--- lib/app/services/api_client_srv.dart
+++ lib/app/services/api_client_srv.dart
@@ -1,19 +1,84 @@
 import 'package:dio/dio.dart';
+import 'package:get_storage/get_storage.dart';
 
 class ApiClient {
   final String baseURL = 'http://35.198.180.32';
+  final box = GetStorage();
 
-  Future connectStreamRooms(int roomId) async {
+  String getCurUserToken() {
+    String token = box.read('cur_user_token');
+    return token;
+  }
+
+  Future connectStreamRooms(int roomId, bool watch) async {
     String apiPath = '/api/stream-rooms/';
     String apiUrl = this.baseURL + apiPath + roomId.toString();
-    Response response = await Dio().patch(apiUrl);
-    return response;
+    //--AUTH set:
+    String curUserToken = getCurUserToken();
+    Response response = await Dio().patch(apiUrl,
+        data: {'watch': watch},
+        options: Options(
+          headers: {"Authorization": "Bearer $curUserToken"},
+        ));
+    return response.data;
   }
 
   Future getStreamUsers(int roomId) async {
     String apiPath = '/api/stream-rooms/';
     String apiUrl = this.baseURL + apiPath + roomId.toString();
+    //--AUTH set:
+    String curUserToken = getCurUserToken();
+    Response response = await Dio().get(apiUrl,
+        options: Options(
+          headers: {"Authorization": "Bearer $curUserToken"},
+        ));
+    return response.data;
+  }
+
+  Future getStreamRooms() async {
+    String apiPath = '/api/stream-rooms/';
+    String apiUrl = this.baseURL + apiPath;
+    //--AUTH set:
     Response response = await Dio().get(apiUrl);
-    return response;
+    return response.data;
+  }
+
+  Future getStreamRoomById(int roomId) async {
+    String apiPath = '/api/stream-rooms/';
+    String apiUrl = this.baseURL + apiPath + roomId.toString();
+    //--AUTH set:
+    String curUserToken = getCurUserToken();
+    Response response = await Dio().get(apiUrl,
+        options: Options(
+          headers: {"Authorization": "Bearer $curUserToken"},
+        ));
+    return response.data;
+  }
+
+  Future<Map<String, dynamic>> registerStreamUser(String email, String password) async {
+    String apiPath = '/api/users/';
+    String apiUrl = this.baseURL + apiPath;
+    Response response = await Dio().post(apiUrl, data: {'email': email, 'password': password});
+    return response.data;
+  }
+
+  Future<Map<String, dynamic>> getUserToken(String email, String password) async {
+    String apiPath = '/api/session/';
+    String apiUrl = this.baseURL + apiPath;
+    Response response = await Dio().post(apiUrl, data: {'email': email, 'password': password});
+    return response.data; //token
+  }
+
+  Future<Map<String, dynamic>> createStreamRoom(String streamSrc) async {
+    String apiPath = '/api/stream-rooms/';
+    String apiUrl = this.baseURL + apiPath;
+    //--AUTH set:
+    String curUserToken = getCurUserToken();
+    Response response = await Dio().post(apiUrl,
+        data: {'stream_src': streamSrc},
+        options: Options(
+          headers: {"Authorization": "Bearer $curUserToken"},
+        ));
+    return response.data; //stream_src;
   }
 }
--- lib/app/services/stream_srv.dart
+++ lib/app/services/stream_srv.dart
@@ -1,8 +1,10 @@
 import 'package:ayas_mobile/app/models/user.dart';
 import 'package:ayas_mobile/app/services/api_client_srv.dart';
+import 'package:get_storage/get_storage.dart';
 
 class StreamService {
   final pooling_time = 1; //--sec-
+  final box = GetStorage();
 
   connectStreem() {}
 
@@ -13,5 +15,22 @@ class StreamService {
       print("API stream Error: ${e.toString()}");
       return null;
     }
+  }
+
+  Future<bool> registerStreamUser() async {
+    try {
+      String curUserEmail = box.read('cur_user_email');
+      String curUserPassword = box.read('cur_user_password');
+      var data = await ApiClient().registerStreamUser(curUserEmail, curUserPassword);
+      if (data['id'] != null) {
+        print("---registerStreamUser:$data['id']");
+        box.write('cur_user_id', data['id'].toString());
+        return true;
+      }
+    } catch (e) {
+      print("API stream Error: ${e.toString()}");
+      return false;
+    }
+    return false;
   }
 }
